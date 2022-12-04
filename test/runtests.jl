c584daa83a08d2836f4394eddfe6da0b6dd2d649
Keno Fischer <keno@juliacomputing.com>
2022-12-04 02:37:11 +0000

    Make test conditional

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,8 @@ using Test
     @testset "simple" include("simple.jl")
     @testset "math" include("math.jl")
     @testset "misc" include("misc.jl")
-    @testset "abstract" include("abstract.jl")
+    if VERSION >= v"1.10.0-DEV.90"
+        # This interface depends on julia#47749
+        @testset "abstract" include("abstract.jl")
+    end
 end
