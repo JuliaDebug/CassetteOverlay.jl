d4c766e22cfb5d2a045a72e54ee771fc71309b40
Keno Fischer <keno@juliahub.com>
2024-04-01 23:53:46 +0000

    1.12 adjustments

--- test/misc.jl
+++ test/misc.jl
@@ -48,9 +48,9 @@ end
 @test pass(sparam_isdefined, 42)
 
 if isdefined(Base, :ScopedValues)
-    const sval = ScopedValue(1)
+    const sval = Base.ScopedValue(1)
     @test pass() do
-        with(sval => 2) do
+        Base.with(sval => 2) do
             sval[]
         end
     end == 2
