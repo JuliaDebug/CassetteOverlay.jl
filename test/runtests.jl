ede096528027ffdd2d967deff1af2d4e235497f4
Shuhei Kadowaki <aviatesk@gmail.com>
2024-04-02 21:38:10 +0900

    add `@static` annotation

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Test
     @testset "simple" include("simple.jl")
     @testset "math" include("math.jl")
     @testset "misc" include("misc.jl")
-    if VERSION >= v"1.10.0-DEV.90"
+    @static if VERSION >= v"1.10.0-DEV.90"
         # This interface depends on julia#47749
         @testset "abstract" include("abstract.jl")
     end
